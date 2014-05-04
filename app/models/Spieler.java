b67f520d8cb2c37158e8215d2da01a5f0ad01202
mokocha <jozef.mesaros@gmail.com>
2014-05-04 18:24:55 +0200

    spieler

--- app/models/Spieler.java
+++ app/models/Spieler.java
@@ -1,6 +1,7 @@
 package models;
 
 import play.data.validation.Constraints;
+import play.db.jpa.JPA;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
@@ -14,22 +15,22 @@ public class Spieler {
 	@Id
 	public long id;
 	 
+    public String vorname;
+
+    public String nachname;
+
+    public String birthday;
+
+    public enum geschlecht { male, female };
+
     @Constraints.Required
     @Constraints.MinLength(4)
     @Constraints.MaxLength(16)
-    public String name;
+    public String username;
 
     @Constraints.Required
     public String password;
 
-    
-    public void save() {
-        if(this.Spieler.id == null) {
-            this.Spieler = null;
-        } else {
-            this.Spieler = Spieler.findById(Spieler.id);
-        }
-        JPA.em().persist(this);
-    }
+
 
 }
